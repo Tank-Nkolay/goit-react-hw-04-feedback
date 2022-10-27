01d1b4399ba24569d8c025835e2271d6cf57ffe1
Nikolay Tankov <tankov.n.k@gmail.com>
2022-10-27 09:27:53 +0300

    1

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 // import PropTypes from 'prop-types';
 import Section from '../Section/Section';
 import FeedbackOptions from '../FeedbackOptions/FeedbackOptions';
@@ -6,78 +6,67 @@ import Statistics from '../Statistics/Statistics';
 import Notification from '../Notification/Notification';
 import { Markup, Title, TitleStatistics } from './Feedback.styled.jsx';
 
-class Feedback extends React.Component {
+export default function Feedback() {
   // Передаем стартовое значение ===
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const types = { good, neutral, bad };
+
+  const stateKeys = Object.keys(types);
+
+  const handleFeedbackСlick = option => {
+    switch (option) {
+      case 'good':
+        setGood(prevGood => prevGood + 1);
+        break;
+
+      case 'neutral':
+        setNeutral(prevNeutral => prevNeutral + 1);
+        break;
+
+      case 'bad':
+        setBad(prevBad => prevBad + 1);
+        break;
+
+      default:
+        break;
+    }
   };
 
-  stateKeys = Object.keys(this.state);
-
-  options = this.stateKeys;
-
-  handleFeedbackСlick = option => {
-    this.setState(prevState => ({
-      [option]: prevState[option] + 1,
-    }));
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
   };
 
-  countTotalFeedback = () => {
-    return Object.values(this.state).reduce((acc, value) => acc + value, 0);
+  const countPositiveFeedbackPercentage = () => {
+    return Math.floor((good / countTotalFeedback()) * 100);
   };
 
-  countPositiveFeedbackPercentage = () => {
-    return Math.floor((this.state.good / this.countTotalFeedback()) * 100);
-  };
-  // ==============================================
+  return (
+    <Markup>
+      <Title>Please leave feedback</Title>
+      <Section title="">
+        <FeedbackOptions
+          options={stateKeys}
+          onLeaveFeedback={handleFeedbackСlick}
+        />
+      </Section>
 
-  // {this.state.visible ? 'Скрыть' : 'Показать'}
-  render() {
-    const { good, neutral, bad } = this.state;
-
-    return (
-      <Markup>
-        <Title>Please leave feedback</Title>
-        <Section title="">
-          <FeedbackOptions
-            options={this.stateKeys}
-            onLeaveFeedback={this.handleFeedbackСlick}
+      <TitleStatistics>Statistics</TitleStatistics>
+      <Section title="">
+        {countTotalFeedback() > '0' ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
           />
-        </Section>
-
-        <TitleStatistics>Statistics</TitleStatistics>
-        <Section title="">
-          {this.countTotalFeedback() > '0' ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          ) : (
-            <Notification message="There is no feedback" />
-          )}
-        </Section>
-      </Markup>
-    );
-  }
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </Markup>
+  );
 }
-
-export default Feedback;
-
-// ===================================
-// countPositiveFeedbackPercentage() {
-//   return parseInt((this.state.good / this.countTotalFeedback()) * 100);
-// }
-// ===================================
-// ДЕФОЛТНОЕ значение ПРОПС
-//   static defaultProps = {
-//     initialValue: 5,
-//   };
-// static propTypes = {
-//   //
-// };
-// ===================================
